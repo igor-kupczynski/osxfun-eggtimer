021aa98c79b87c3cb6626cbd1405ff99dc9eb04f
Igor Kupczyński <igor@kupczynski.info>
2017-08-15 22:43:15 +0200

    Initial Commit

---
+++ EggTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EggTimer
+//
+//  Created by Igor Kupczyński on 15/08/2017.
+//  Copyright © 2017 Igor Kupczynski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Igor Kupczyński on 15/08/2017.
+//  Copyright © 2017 Igor Kupczynski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
