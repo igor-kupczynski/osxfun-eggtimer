af61b056713f44b3f11dc731ece64d698fd49cfa
Igor Kupczyński <igor@kupczynski.info>
2017-08-17 00:15:02 +0200

    Basic app logic

--- EggTimer/AppDelegate.swift
+++ EggTimer/AppDelegate.swift
@@ -11,17 +11,25 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var startTimerMenuButton: NSMenuItem!
-    @IBOutlet weak var stopTimerMenuButton: NSMenuItem!
-    @IBOutlet weak var resetTimerMenuButton: NSMenuItem!
+    
+
+    @IBOutlet weak var startTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var stopTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var resetTimerMenuItem: NSMenuItem!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        enableMenus(start: true, stop: false, reset: false)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func enableMenus(start: Bool, stop: Bool, reset: Bool) {
+        startTimerMenuItem.isEnabled = start
+        stopTimerMenuItem.isEnabled = stop
+        resetTimerMenuItem.isEnabled = reset
     }
 
 
