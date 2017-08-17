f438948e17e34291b02af6d3f1e490b40265721f
Igor Kupczyński <igor@kupczynski.info>
2017-08-17 19:36:09 +0200

    Ask if really want to update the preferences

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -162,10 +162,8 @@ extension ViewController {
         updateDisplay(for: prefs.selectedTime)
         
         let notificationName = Notification.Name(rawValue: "PrefsChanged")
-        NotificationCenter.default.addObserver(forName: notificationName,
-                                               object: nil, queue: nil) {
-                                                (notification) in
-                                                self.updateFromPrefs()
+        NotificationCenter.default.addObserver(forName: notificationName, object: nil, queue: nil) {
+            (notification) in self.checkForResetAfterPrefsChange()
         }
     }
     
@@ -174,4 +172,22 @@ extension ViewController {
         self.resetButtonClicked(self)
     }
     
+    func checkForResetAfterPrefsChange() {
+        if eggTimer.isStopped || eggTimer.isPaused {
+            updateFromPrefs()
+        } else {
+            let alert = NSAlert()
+            alert.messageText = "Reset timer with the new settings?"
+            alert.informativeText = "This will stop your current timer!"
+            alert.alertStyle = .warning
+            
+            alert.addButton(withTitle: "Reset")
+            alert.addButton(withTitle: "Cancel")
+            
+            let response = alert.runModal()
+            if response == NSAlertFirstButtonReturn {
+                self.updateFromPrefs()
+            }
+        }
+    }
 }
